4a68ae1152c59391c5ebf282abbeed9ec4095476
Neha Nikhat <nehanikhat18@gmail.com>
2021-09-27 23:55:39 +0530

    fix error

--- src/components/productsList.js
+++ src/components/productsList.js
@@ -43,7 +43,7 @@ export default function ProductsList() {
 
   useEffect(() => {
     getProductsList();
-  }, [products]);
+  });
 
   if (prodName !== "") {
     return <ProductByName productName={prodName} toggleProductName={setProdName} />;
